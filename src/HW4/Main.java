1634671168eb7a83bd7c20ca6cdbd3483ad7b282
SabaevaEvgeniya <eva230391@mail.ru>
2023-10-18 01:10:15 +0400

    added changes in the folder 'HW4'

--- src/HW4/Main.java
+++ src/HW4/Main.java
@@ -6,15 +6,10 @@ public class Main {
         TeacherService Validate = new TeacherService();
         TeacherController Controller = new TeacherController(Validate, View);
 
-//        Controller.addTeacher("Jon", "Math");
-//        Controller.addTeacher("Bell", "English");
-//        Controller.addTeacher("Roman", "Programming");
         Controller.createTeacher("Фролов Иван Викторович", "Математика");
         Controller.createTeacher("Васильева Ольга Семеновна", "Русский язык");
         Controller.createTeacher("Коврова Ирина Анатольевна", "История");
 
-
-//        Controller.TeacherView();
         Controller.viewTeacherList();
     }
 
--- src/HW4/TeacherService.java
+++ src/HW4/TeacherService.java
@@ -2,12 +2,12 @@ package HW4;
 
 public class TeacherService {
     public boolean validateName(Teacher teacher) {
-       // return name != null && !name.isEmpty();
+
         return teacher.getName() != null && !teacher.getName().isEmpty();
     }
 
     public boolean validateSubject(Teacher teacher) {
-        //return subject != null && !subject.isEmpty();
+
         return  teacher.getSubject() != null && !teacher.getSubject().isEmpty();
     }
 
